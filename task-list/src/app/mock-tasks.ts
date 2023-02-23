e5dfcd1b524377fabcead9831019e9cdbafb454e
SantiMendezz <santimendez@gmail.com>
2023-02-23 19:41:35 -0300

    Agragar mock e interface de la lista de tareas

---
+++ task-list/src/app/mock-tasks.ts
@@ -0,0 +1,27 @@
+import {Task} from "./Task"
+export const TASKS:Task[] = [
+    {
+        id:1,
+        text: "Terminar el primer modulo de Angular",
+        day: 'Febrero 26 a las 12:00',
+        reminder: true,
+    },
+    {
+        id:2,
+        text: "Hacer las compras",
+        day: 'Febrero 26 a las 17:00',
+        reminder: true,
+    },
+    {
+        id:3,
+        text: "Leer sobre el curso de CSS",
+        day: 'Febrero 26 a las 19:00',
+        reminder: false,
+    },
+    {
+        id:3,
+        text: "Continuar con la lectura del libro",
+        day: 'Febrero 26 a las 21:00',
+        reminder: false,
+    }
+]
