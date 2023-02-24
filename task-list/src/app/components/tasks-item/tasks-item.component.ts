ee2dfb1558e7763b39c463e261ebbc49acc9dafb
SantiMendezz <santimendez@gmail.com>
2023-02-24 10:23:01 -0300

    Agregamos tasklist y funcionalidades

--- task-list/src/app/components/tasks-item/tasks-item.component.ts
+++ task-list/src/app/components/tasks-item/tasks-item.component.ts
@@ -2,6 +2,8 @@ import { Component, Input } from '@angular/core';
 import { Task } from 'src/app/Task';
 import { TASKS } from 'src/app/mock-tasks';
 
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+
 @Component({
   selector: 'app-tasks-item',
   templateUrl: './tasks-item.component.html',
@@ -9,4 +11,6 @@ import { TASKS } from 'src/app/mock-tasks';
 })
 export class TasksItemComponent {
   @Input() task: Task = TASKS[0]
+  faTimes = faTimes;
+
 }
